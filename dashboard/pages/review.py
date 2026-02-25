9560e6ef01e1f1fb05ef9863cc7669b2fa897bd4
sigvardbratlie <sigvard.bratlie@gmail.com>
2026-02-25 10:02:50 +0100

    refactor: remove seasonal and yearly review pages, update review.py to include sidebar setup

--- dashboard/pages/review.py
+++ dashboard/pages/review.py
@@ -1,9 +1,10 @@
 import streamlit as st
 from dashboard import init
-from components import SeasonalReviewComponent,YearlyReviewComponent
+from components import SeasonalReviewComponent,YearlyReviewComponent,SidebarComponent
 
 init()
 
+SidebarComponent().sidebar_setup(disable_seasonpicker=True,disable_datepicker=True, disable_custom_datepicker=True)
 
 tabs = st.tabs(["seasonal", "yearly"])
 with tabs[0]:
